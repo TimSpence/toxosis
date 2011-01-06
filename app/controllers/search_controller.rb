e2523adb61c0dcbd484c8f82ce7340cf1000ede9
Chris Saylor <cwsaylor@gmail.com>
2011-01-06 11:17:20 -0800

    Adding rudimentary pagination.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,6 @@ class SearchController < ApplicationController
     # Call out to indextank api
     client = IndexTank::Client.new(ENV['INDEXTANK_API_URL'])
     index = client.indexes('idx')
-    @results = index.search(params['q'])
+    @results = index.search(params['q'], :start => params.fetch('start' , 1))
   end
 end
