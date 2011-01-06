69d9ff300ee2b8aec0469a192cb27ac781285aff
Chris Saylor <cwsaylor@gmail.com>
2011-01-05 22:36:26 -0800

    Add search query and text field to index document.

--- app/models/spill.rb
+++ app/models/spill.rb
@@ -3,6 +3,7 @@ class Spill
 
   def to_document 
     {
+      :text =>  self['Facility Name'] + " " + self['Chemical'] + " " + self['ZIP'] + " " + self['City'] + " " + self['ST']
       :id => self._id,
       :facility_name => self['Facility Name'],
       :chemical => self['Chemical'],
