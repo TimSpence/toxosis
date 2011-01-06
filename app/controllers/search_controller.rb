4f126a88243db96912b2f41a5d022d4cdbab43a3
Chris Saylor <cwsaylor@gmail.com>
2011-01-05 22:42:35 -0800

    Cleaning up

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,8 @@
 class SearchController < ApplicationController
   def index
     # Call out to indextank api
-    client = IndexTank::Client.new(ENV['INDEXTANK_API_URL'] || 'http://:ru9qFNqdHsqIvt@z9m8.api.indextank.com')
-    index = client.indexes('lulz')
+    client = IndexTank::Client.new(ENV['INDEXTANK_API_URL'])
+    index = client.indexes('idx')
     @results = index.search(params['q'])
-    
-    
   end
 end
