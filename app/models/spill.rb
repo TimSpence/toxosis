ab29eed0b8bc908f397960e7cad2a37ca26ea150
= <=>
2011-01-05 21:26:45 -0800

    added rake task for importing into indextank

--- app/models/spill.rb
+++ app/models/spill.rb
@@ -1,3 +1,15 @@
 class Spill
   include Mongoid::Document
+
+  def to_document 
+    {
+      :id => self._id,
+      :facility_name => self['Facility Name'],
+      :chemical => self['Chemical'],
+      :zip => self['ZIP'],
+      :city => self['City'],
+      :state => self['ST']
+    }
+  end
+
 end
